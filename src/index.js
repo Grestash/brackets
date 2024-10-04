20d647e00f233bf13243a140c4d3f23c90b01c93
Pavel Shliatski <pavelshlatcki1@gmail.com>
2024-10-04 22:23:17 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let bracketsPair = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    bracketsPair[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (stack.length !== 0 && bracketsPair[str[i]] === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+  return stack.length === 0;
+};
